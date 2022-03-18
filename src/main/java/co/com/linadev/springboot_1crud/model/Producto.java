303e5dfd655fff6711a811a2ec2efb4ad71c9ccb
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-18 17:38:55 -0500

    se crea modelo product

---
+++ src/main/java/co/com/linadev/springboot_1crud/model/Producto.java
@@ -0,0 +1,24 @@
+package co.com.linadev.springboot_1crud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private Float price;
+}
