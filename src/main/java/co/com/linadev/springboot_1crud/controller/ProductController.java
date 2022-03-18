0edea1fc5f4a37b9366ab7bddf075bb82ce39cbb
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-18 18:54:21 -0500

    se prueban endpoints en Insomnia

--- src/main/java/co/com/linadev/springboot_1crud/controller/ProductController.java
+++ src/main/java/co/com/linadev/springboot_1crud/controller/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -35,5 +36,26 @@ public class ProductController {
         return productService.saveProduct(product);
     }
 
-    // get product by id
+    // get product by id (Float)
+    // localhost:8080/api/product/findById/{id}
+    @GetMapping("/findById/{id}")
+    public Optional<Product> getProductById(@PathVariable Integer id){
+        return productService.getProductById(id);
+    }
+
+    // get product by id (String)
+    // localhost:8080/api/product/findProductId/{id}
+    @GetMapping("/findProductId/{id}")
+    public Product findByProductId(@PathVariable String id){
+        return productService.findByProductId(id);
+    }
+
+    //find by productName
+    // localhost:8080/api/product/findByName/{name}
+    @GetMapping("/findByName/{name}")
+    public List<Product> findAllByProductName(@PathVariable String name){
+        return productService.findAllByProductName(name);
+    }
+
+
 }
