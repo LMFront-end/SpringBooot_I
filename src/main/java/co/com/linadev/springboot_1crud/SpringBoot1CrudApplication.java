5101aa757a4a62ae664716f7014aeac1a4c52655
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-18 17:30:56 -0500

    se crea conexion con bd

---
+++ src/main/java/co/com/linadev/springboot_1crud/SpringBoot1CrudApplication.java
@@ -0,0 +1,13 @@
+package co.com.linadev.springboot_1crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1CrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot1CrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/linadev/springboot_1crud/SpringBoot1CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.linadev.springboot_1crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
